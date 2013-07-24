6e6fa51a69840995751a3bf121847eab5c3d9af9
Brandon Donnelson <branflake2267@gmail.com>
2013-07-23 19:37:46 -0700

    Update Blank.java

--- MyJavaProject/src/com/gonevertical/test/Blank.java
+++ MyJavaProject/src/com/gonevertical/test/Blank.java
@@ -5,4 +5,12 @@ public class Blank {
     private void run() {
         System.out.println("fast");
     }
+    
+    public void newMethodThree() {
+    	System.out.println("I've been committed to master.");
+		
+		if (true) {
+			System.out.print("Maybe...");
+		}
+	}
 }
